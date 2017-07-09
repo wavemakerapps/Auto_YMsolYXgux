51bd19062b76ad55ab5ff03f7c1d67f0498fb611
root <root@61949b3d2f95>
2017-07-09 03:12:12 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_YMsolYXgux",
+  "homePage" : "Main",
+  "name" : "Auto_YMsolYXgux",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
